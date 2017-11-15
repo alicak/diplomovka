93841c854672dfb77def585a722ececfc08bd8b4
Alica Kacengova <>
2017-11-15 17:09:01 +0100

    fix small error

--- src/sk/upjs/ics/diplomovka/base/CrossoverBase.java
+++ src/sk/upjs/ics/diplomovka/base/CrossoverBase.java
@@ -1,6 +1,6 @@
 package sk.upjs.ics.diplomovka.base;
 
-public abstract class CrossoverBase<ChromosomeType> {
+public abstract class CrossoverBase<ChromosomeType extends ChromosomeBase> {
 
     public abstract ChromosomePair<ChromosomeType> doCrossover(ChromosomeType chromosome1, ChromosomeType chromosome2);
 
