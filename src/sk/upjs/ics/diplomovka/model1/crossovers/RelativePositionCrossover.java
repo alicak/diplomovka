6f154dd2d7bb6fcf2e80c7a4499508cee1562241
Alica Kacengova <alica.kacengova@gmail.com>
2017-11-27 18:17:20 +0100

    add some steps to relative crossover

--- src/sk/upjs/ics/diplomovka/model1/crossovers/RelativePositionCrossover.java
+++ src/sk/upjs/ics/diplomovka/model1/crossovers/RelativePositionCrossover.java
@@ -5,6 +5,7 @@ import sk.upjs.ics.diplomovka.base.CrossoverBase;
 import sk.upjs.ics.diplomovka.model1.chromosomes.RelativePositionChromosome;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RelativePositionCrossover extends CrossoverBase {
@@ -19,7 +20,7 @@ public class RelativePositionCrossover extends CrossoverBase {
 
     private List<Chromosome> doRelativeCrossover(RelativePositionChromosome chromosome1, RelativePositionChromosome chromosome2) {
         RelativePositionChromosome newChromosome = locateCommonGenes(chromosome1, chromosome2);
-        assignGates(newChromosome);
+        assignGates(chromosome1, chromosome2, newChromosome);
         indicateInfeasibleGenes(newChromosome);
         produceOffspring(newChromosome);
         List<Chromosome> offspring = new ArrayList<>();
@@ -29,39 +30,93 @@ public class RelativePositionCrossover extends CrossoverBase {
 
     // step 1
     private RelativePositionChromosome locateCommonGenes(RelativePositionChromosome chromosome1, RelativePositionChromosome chromosome2) {
-        List<Integer> genes = new ArrayList<>();
         int flights = chromosome1.getNoFlights();
-
         RelativePositionChromosome result = new RelativePositionChromosome(chromosome1.getNoOfGates(), flights);
+
+        Integer[] genesArray = new Integer[flights * flights + flights];
+        Arrays.fill(genesArray, 0);
+        result.setGenes(Arrays.asList(genesArray));
 
         for (int i = 0; i < flights; i++) {
             for (int j = 0; j < flights; j++) {
-                int idx = result.getIndex(i, j);
-                genes.set(idx, chromosome1.getGene(i, j) * chromosome2.getGene(i, j));
+                result.setGene(i, j, chromosome1.getGene(i, j) * chromosome2.getGene(i, j));
             }
         }
 
-        int idx = result.getIndex(flights, 0);
-        for (int j = idx; j < idx + flights; j++) {
+        for (int j = 0; j < flights; j++) {
             int value = 0;
-            if (chromosome1.getGene(j) == chromosome2.getGene(j)) {
-                value = chromosome1.getGene(j);
+            if (chromosome1.getGene(flights, j) == chromosome2.getGene(flights, j)) {
+                value = chromosome1.getGene(flights, j);
             }
-            genes.set(j, value);
+            result.setGene(flights, j, value);
         }
 
-        result.setGenes(genes);
+
         return result;
     }
 
     // step 2
-    private void assignGates(RelativePositionChromosome chromosome) {
-        // TODO
+    private void assignGates(RelativePositionChromosome chromosome1, RelativePositionChromosome chromosome2, RelativePositionChromosome result) {
+        int flights = result.getNoFlights();
+
+        for (int j = 0; j < flights; j++) {
+            if (result.getGene(flights, j) == 0) {
+                continue;
+            }
+
+            if (Math.random() < 0.5) {
+                result.setGene(flights, j, chromosome1.getGene(flights, j));
+            } else {
+                result.setGene(flights, j, chromosome2.getGene(flights, j));
+            }
+        }
+
+        // TODO: every gate must have first dwelling
     }
 
     // step 3
     private void indicateInfeasibleGenes(RelativePositionChromosome chromosome) {
-        // TODO
+        int flights = chromosome.getNoFlights();
+
+        for (int i = 0; i < flights; i++) {
+            int gene = chromosome.getGene(i, i);
+
+            if (gene == 0) {
+                chromosome.setGene(i, i, -1);
+            } else if (gene == 1) {
+
+                for (int m = 0; m < flights; m++) {
+                    if (chromosome.getGene(m, i) == 0) {
+                        chromosome.setGene(m, i, -1);
+                    }
+                }
+
+            } else {
+                throw new IllegalStateException("Value in gene can be only 0 or 1.");
+            }
+        }
+
+        for (int i = 0; i < flights; i++) {
+            for (int j = 0; j < flights; j++) {
+                if (i == j)
+                    continue;
+
+                if (chromosome.getGene(i, j) == 1) {
+
+                    for (int m = 0; m < flights; m++) {
+
+                        if (chromosome.getGene(m, j) == 0) {
+                            chromosome.setGene(m, j, -1);
+                        }
+
+                        if (chromosome.getGene(i, m) == 0) {
+                            chromosome.setGene(i, m, -1);
+                        }
+                    }
+
+                }
+            }
+        }
     }
 
     // step 4
