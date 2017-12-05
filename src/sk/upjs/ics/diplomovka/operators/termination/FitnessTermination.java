8d645e83252c77f4f663b9caadd0a4590518dc34
Alica Kacengova <alica.kacengova@gmail.com>
2017-12-05 23:52:43 +0100

    fix error in FitnessTermination

--- src/sk/upjs/ics/diplomovka/operators/termination/FitnessTermination.java
+++ src/sk/upjs/ics/diplomovka/operators/termination/FitnessTermination.java
@@ -1,22 +1,19 @@
 package sk.upjs.ics.diplomovka.operators.termination;
 
 import sk.upjs.ics.diplomovka.base.PopulationBase;
-import sk.upjs.ics.diplomovka.base.SelectionBase;
 import sk.upjs.ics.diplomovka.base.TerminationBase;
 
-public class FitnessTermination extends TerminationBase{
+public class FitnessTermination extends TerminationBase {
     private double targetFitness;
-    private SelectionBase selection;
     private PopulationBase population;
 
-    public FitnessTermination(double targetFitness, SelectionBase selection, PopulationBase population){
+    public FitnessTermination(double targetFitness, PopulationBase population) {
         this.targetFitness = targetFitness;
-        this.selection = selection;
         this.population = population;
     }
 
     @Override
     public boolean isTerminated() {
-        return targetFitness >= selection.bestChromosome(population).getFitness();
+        return targetFitness >= population.bestChromosome().getFitness();
     }
 }
