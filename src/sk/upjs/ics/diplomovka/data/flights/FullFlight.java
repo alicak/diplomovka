dc7297a3ca534d5f7b1065506384694b6b431b43
Alica Kacengova <alica.kacengova@gmail.com>
2018-01-02 00:22:30 +0100

    really add FullFlight

--- src/sk/upjs/ics/diplomovka/data/flights/FullFlight.java
+++ src/sk/upjs/ics/diplomovka/data/flights/FullFlight.java
@@ -1,10 +1,41 @@
 package sk.upjs.ics.diplomovka.data.flights;
 
-public class ExtendedFlight extends Flight {
+public class FullFlight {
 
+    private Flight flight;
+    private int actual;
+    private String gate;
 
+    public FullFlight() {
+    }
 
-    public ExtendedFlight(Flight flight) {
+    public FullFlight(Flight flight, int actualTime, String gate) {
+        this.flight = flight;
+        this.actual = actualTime;
+        this.gate = gate;
+    }
 
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public int getActual() {
+        return actual;
+    }
+
+    public void setActual(int actual) {
+        this.actual = actual;
+    }
+
+    public String getGate() {
+        return gate;
+    }
+
+    public void setGate(String gate) {
+        this.gate = gate;
     }
 }
