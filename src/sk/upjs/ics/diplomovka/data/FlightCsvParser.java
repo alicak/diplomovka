f1d03c7c4c419f02090bfaf2c56afb52348358f5
Alica Kacengova <alica.kacengova@gmail.com>
2018-01-02 00:20:50 +0100

    implement FlightCsvParser

--- src/sk/upjs/ics/diplomovka/data/FlightCsvParser.java
+++ src/sk/upjs/ics/diplomovka/data/FlightCsvParser.java
@@ -11,7 +11,14 @@ import java.util.Map;
 
 public class FlightCsvParser {
 
-    Map<String, Aircraft> aircrafts = new HashMap<>();
+    private static String SEPARATOR = ";";
+    private Map<String, Aircraft> aircrafts = new HashMap<>();
+    private int id = 0;
+    private static int BEFORE_ARRIVAL_SLOT = 30;
+    private static int AFTER_ARRIVAL_SLOT = 30;
+    private static int BEFORE_DEPARTURE_SLOT = 60;
+    private static int AFTER_DEPARTURE_SLOT = 0;
+
 
     public FlightCsvParser(File aircraftFile) throws IOException {
         parseAircrafts(aircraftFile);
@@ -31,6 +38,7 @@ public class FlightCsvParser {
         List<Flight> flights = new ArrayList<>();
 
         BufferedReader reader = new BufferedReader(new FileReader(flightsFile));
+        reader.readLine(); // first line are column headers
         String line = "";
 
         while ((line = reader.readLine()) != null) {
@@ -46,33 +54,83 @@ public class FlightCsvParser {
 
 
     private Aircraft parseAircraft(String aircraftString) {
-        return null;
+        String[] aircraftArray = aircraftString.split(SEPARATOR);
+        String wingspan = aircraftArray[1];
+        wingspan.replace(",", ".");
+        return new Aircraft(aircraftArray[0], Double.parseDouble(wingspan));
     }
 
+    // column structure: Scheduled, Actual, From, Terminal, Baggage claim, Status, Flight No, Aircraft, Aircraft full
     private Flight parseArrival(String arrivalString) {
-        return null;
-    }
+        String[] flightArray = arrivalString.split(SEPARATOR);
 
-    private Flight parseDeparture(String departureString) {
-        return null;
+        Flight flight = new Flight();
+        flight.setId(++id);
+        flight.setStart(Integer.parseInt(flightArray[ArrivalArray.SCHEDULED.ordinal()]) - BEFORE_ARRIVAL_SLOT);
+        flight.setEnd(Integer.parseInt(flightArray[ArrivalArray.SCHEDULED.ordinal()]) + AFTER_ARRIVAL_SLOT);
+        flight.setType(Flight.FlightType.DEPARTURE);
+
+        // TODO add category to data
+//        if(flightArray[ArrivalArray.TERMINAL.ordinal()].equals("1")) {
+//            flight.setCategory(Flight.FlightCategory.NON_SCHENGEN);
+//        }
+//        else{
+//            flight.setCategory(Flight.FlightCategory.SCHENGEN);
+//        }
+
+        flight.setAircraft(aircrafts.get(flightArray[ArrivalArray.AIRCRAFT.ordinal()]));
+        flight.setTurnaroundTime(0); // TODO
+
+        return flight;
     }
 
     // column structure: Scheduled, Actual, To, Terminal, Gate, Status, Flight No, Aircraft, Aircraft full
-    private Flight parseFlight(String flightString) {
-        String[] flightArray = flightString.split(";");
+    private Flight parseDeparture(String departureString) {
+        String[] flightArray = departureString.split(SEPARATOR);
+
         Flight flight = new Flight();
-        flight.setId();
+        flight.setId(++id);
+        flight.setStart(Integer.parseInt(flightArray[DepartureArray.SCHEDULED.ordinal()]) - BEFORE_DEPARTURE_SLOT);
+        flight.setEnd(Integer.parseInt(flightArray[DepartureArray.SCHEDULED.ordinal()]) + AFTER_DEPARTURE_SLOT);
+        flight.setType(Flight.FlightType.DEPARTURE);
 
         // TODO add category to data
-        if(flightArray[3].equals("1")) {
-            flight.setCategory(Flight.FlightCategory.NON_SCHENGEN);
-        }
-        else if(flightArray[3].equals("2")) {
-            flight.setCategory(Flight.FlightCategory.SCHENGEN);
+//        if(flightArray[DepartureArray.TERMINAL.ordinal()].equals("1")) {
+//            flight.setCategory(Flight.FlightCategory.NON_SCHENGEN);
+//        }
+//        else{
+//            flight.setCategory(Flight.FlightCategory.SCHENGEN);
+//        }
 
-        }
+        flight.setAircraft(aircrafts.get(flightArray[DepartureArray.AIRCRAFT.ordinal()]));
+        flight.setTurnaroundTime(0); // TODO
 
-        flight.setAircraft(aircrafts.get(flightArray[8]));
-        return null;
+        return flight;
     }
+
+    private enum DepartureArray {
+        SCHEDULED,
+        ACTUAL,
+        TO,
+        TERMINAL,
+        GATE,
+        STATUS,
+        FLIGHT_NO,
+        AIRCRAFT,
+        AIRCRAFT_FULL
+    }
+
+    private enum ArrivalArray {
+        SCHEDULED,
+        ACTUAL,
+        FROM,
+        TERMINAL,
+        BAGGAGE_CLAIM,
+        STATUS,
+        FLIGHT_NO,
+        AIRCRAFT,
+        AIRCRAFT_FULL
+    }
+
+
 }
