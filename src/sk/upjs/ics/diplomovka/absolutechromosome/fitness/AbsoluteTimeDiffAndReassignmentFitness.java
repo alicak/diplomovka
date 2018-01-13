0ba9e94b81891816defe24f74fb80e2f47a40f2d
Alica Kacengova <alica.kacengova@gmail.com>
2018-01-13 16:59:13 +0100

    adjust TimeDiffAndReassignmentFitness

--- src/sk/upjs/ics/diplomovka/absolutechromosome/fitness/AbsoluteTimeDiffAndReassignmentFitness.java
+++ src/sk/upjs/ics/diplomovka/absolutechromosome/fitness/AbsoluteTimeDiffAndReassignmentFitness.java
@@ -6,19 +6,17 @@ import sk.upjs.ics.diplomovka.data.flights.FlightStorage;
 import sk.upjs.ics.diplomovka.data.stands.StandsStorage;
 
 public class AbsoluteTimeDiffAndReassignmentFitness extends FitnessFunctionBase {
-    private StandsStorage standsStorage;
     private AbsoluteReassignmentFitness reassignmentFitness;
     private AbsoluteTimeDiffFitness timeDiffFitness;
 
     public AbsoluteTimeDiffAndReassignmentFitness(FlightStorage flightStorage, StandsStorage standsStorage) {
         super(flightStorage);
-        this.standsStorage = standsStorage;
         reassignmentFitness = new AbsoluteReassignmentFitness(flightStorage, standsStorage);
         timeDiffFitness = new AbsoluteTimeDiffFitness(flightStorage);
     }
 
     @Override
     public double calculateFitness(Chromosome chromosome) {
-        return timeDiffFitness.calculateFitness(chromosome) * reassignmentFitness.calculateFitness(chromosome);
+        return timeDiffFitness.calculateFitness(chromosome) + 60 * reassignmentFitness.calculateFitness(chromosome);
     }
 }
--- src/sk/upjs/ics/diplomovka/simplechromosome/fitness/SimpleTimeDiffAndReassignmentFitness.java
+++ src/sk/upjs/ics/diplomovka/simplechromosome/fitness/SimpleTimeDiffAndReassignmentFitness.java
@@ -6,19 +6,17 @@ import sk.upjs.ics.diplomovka.data.flights.FlightStorage;
 import sk.upjs.ics.diplomovka.data.stands.StandsStorage;
 
 public class SimpleTimeDiffAndReassignmentFitness extends FitnessFunctionBase {
-    private StandsStorage standsStorage;
     private SimpleReassignmentFitness reassignmentFitness;
     private SimpleTimeDiffFitness timeDiffFitness;
 
     public SimpleTimeDiffAndReassignmentFitness(FlightStorage flightStorage, StandsStorage standsStorage) {
         super(flightStorage);
-        this.standsStorage = standsStorage;
         reassignmentFitness = new SimpleReassignmentFitness(flightStorage, standsStorage);
         timeDiffFitness = new SimpleTimeDiffFitness(flightStorage);
     }
 
     @Override
     public double calculateFitness(Chromosome chromosome) {
-        return timeDiffFitness.calculateFitness(chromosome) * reassignmentFitness.calculateFitness(chromosome);
+        return timeDiffFitness.calculateFitness(chromosome) + 60 * reassignmentFitness.calculateFitness(chromosome);
     }
 }
