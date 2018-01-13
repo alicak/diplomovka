f014f002e10d70a3ca60801e9f7bd3096f9bb949
Alica Kacengova <alica.kacengova@gmail.com>
2018-01-13 02:35:37 +0100

    implement SimpleChromosomeGenerator

--- src/sk/upjs/ics/diplomovka/simplechromosome/SimpleChromosome.java
+++ src/sk/upjs/ics/diplomovka/simplechromosome/SimpleChromosome.java
@@ -2,19 +2,24 @@ package sk.upjs.ics.diplomovka.simplechromosome;
 
 import sk.upjs.ics.diplomovka.base.Chromosome;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleChromosome extends Chromosome {
     private int noOfGates;
     private SimpleFeasibilityChecker feasibilityChecker;
 
+    public SimpleChromosome(int noOfGates) {
+        this.noOfGates = noOfGates;
+    }
+
     public SimpleChromosome(List<Integer> genes, int noOfGates) {
         super(genes);
         this.noOfGates = noOfGates;
     }
 
-    public SimpleFeasibilityChecker getFeasibilityChecker() {
-        return feasibilityChecker;
+    public void setFeasibilityChecker(SimpleFeasibilityChecker feasibilityChecker) {
+        this.feasibilityChecker = feasibilityChecker;
     }
 
     @Override
@@ -37,5 +42,16 @@ public class SimpleChromosome extends Chromosome {
 
     public int getNoOfGates() {
         return noOfGates;
+    }
+
+    public SimpleChromosome copy() {
+        SimpleChromosome chromosome = new SimpleChromosome(noOfGates);
+
+        chromosome.setFitness(getFitness());
+        List<Integer> genes = new ArrayList<>(getGenes());
+        chromosome.setGenes(genes);
+        chromosome.setFeasibilityChecker(feasibilityChecker);
+
+        return chromosome;
     }
 }
