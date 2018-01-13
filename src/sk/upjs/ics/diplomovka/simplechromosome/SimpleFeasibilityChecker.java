c8da827b8a6a2a64dced53c8d14eb3eb3f787275
Alica Kacengova <alica.kacengova@gmail.com>
2018-01-13 01:30:15 +0100

    implement SimpleFeasibilityChecker

--- src/sk/upjs/ics/diplomovka/simplechromosome/SimpleFeasibilityChecker.java
+++ src/sk/upjs/ics/diplomovka/simplechromosome/SimpleFeasibilityChecker.java
@@ -3,6 +3,7 @@ package sk.upjs.ics.diplomovka.simplechromosome;
 import sk.upjs.ics.diplomovka.base.Chromosome;
 import sk.upjs.ics.diplomovka.base.FeasibilityCheckerBase;
 import sk.upjs.ics.diplomovka.data.flights.FlightStorage;
+import sk.upjs.ics.diplomovka.data.stands.AircraftStand;
 import sk.upjs.ics.diplomovka.data.stands.StandsStorage;
 
 public class SimpleFeasibilityChecker extends FeasibilityCheckerBase {
@@ -12,11 +13,17 @@ public class SimpleFeasibilityChecker extends FeasibilityCheckerBase {
 
     @Override
     public boolean checkChromosomeFeasibility(Chromosome chromosome) {
-        throw new UnsupportedOperationException(); // TODO
+        SimpleChromosome c = (SimpleChromosome) chromosome;
+        for (int f = 0; f < c.getLength(); f++) {
+            if(!checkFlightFeasibility(f, c.getGene(f)))
+                return false;
+        }
+        return true;
     }
 
     @Override
     public boolean checkFlightFeasibility(int flightValue, int gate) {
-        throw new UnsupportedOperationException(); // TODO
+        AircraftStand stand = standsStorage.getStandByNumber(gate);
+        return stand.checkFlight(flightStorage.getFlightByNumber(flightValue));
     }
 }
