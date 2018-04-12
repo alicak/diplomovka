6750bcb4c5a8ac446260f96b8327dc4bd2ca2c86
Alica Kacengova <alica.kacengova@gmail.com>
2018-04-12 11:35:00 +0200

    remove simple chromosome for now

--- src/sk/upjs/ics/diplomovka/main/AssignmentCreator.java
+++ src/sk/upjs/ics/diplomovka/main/AssignmentCreator.java
@@ -4,9 +4,6 @@ import sk.upjs.ics.diplomovka.absolutechromosome.AbsolutePositionChromosome;
 import sk.upjs.ics.diplomovka.absolutechromosome.AbsolutePositionFeasibilityChecker;
 import sk.upjs.ics.diplomovka.data.flights.*;
 import sk.upjs.ics.diplomovka.data.stands.StandsStorage;
-import sk.upjs.ics.diplomovka.simplechromosome.SimpleChromosome;
-import sk.upjs.ics.diplomovka.simplechromosome.SimpleFeasibilityChecker;
-
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -38,22 +35,4 @@ public class AssignmentCreator {
 
         return originalAssignment;
     }
-
-    public SimpleChromosome createSimpleOriginalAssignment(SimpleFeasibilityChecker feasibilityChecker) throws IOException {
-        SimpleChromosome originalAssignment = new SimpleChromosome(standsStorage.getNoOfStands());
-        originalAssignment.setFeasibilityChecker(feasibilityChecker);
-
-        Integer[] genesArray = new Integer[flightStorage.getNoOfFlights()];
-        Arrays.fill(genesArray, SimpleChromosome.EMPTY_GENE);
-        originalAssignment.setGenes(Arrays.asList(genesArray));
-
-        for (Flight f : flightStorage.getSortedFlights()) {
-            int standNo = standsStorage.getNumberById(f.getOriginalStandId());
-            int flightNo = flightStorage.getNumberById(f.getId());
-            originalAssignment.setGene(flightNo, standNo);
-        }
-
-        return originalAssignment;
-    }
-
 }
--- src/sk/upjs/ics/diplomovka/main/PopulationCreator.java
+++ src/sk/upjs/ics/diplomovka/main/PopulationCreator.java
@@ -7,11 +7,6 @@ import sk.upjs.ics.diplomovka.absolutechromosome.AbsolutePositionPopulation;
 import sk.upjs.ics.diplomovka.base.Chromosome;
 import sk.upjs.ics.diplomovka.data.flights.FlightStorage;
 import sk.upjs.ics.diplomovka.data.stands.StandsStorage;
-import sk.upjs.ics.diplomovka.simplechromosome.SimpleChromosome;
-import sk.upjs.ics.diplomovka.simplechromosome.SimpleChromosomeGenerator;
-import sk.upjs.ics.diplomovka.simplechromosome.SimpleFeasibilityChecker;
-import sk.upjs.ics.diplomovka.simplechromosome.SimplePopulation;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,24 +33,5 @@ public class PopulationCreator {
         population.prepareForFitnessCalculation(generation);
 
         return population;
-    }
-
-    public static SimplePopulation createSimpleInitialPopulation(int generationSize, SimpleChromosome originalAssignment, SimpleFeasibilityChecker feasibilityChecker) {
-        SimpleChromosomeGenerator generator = new SimpleChromosomeGenerator(originalAssignment.getNoOfGates(), originalAssignment.getNoOfFlights(), feasibilityChecker);
-
-        // first half are random assignments
-        List<Chromosome> generation = new ArrayList<>();
-        for (int i = 0; i < generationSize / 2; i++) {
-            generation.add(generator.generateChromosome());
-        }
-
-        // second half are mutations of original assignment
-        for (int i = generationSize / 2; i < generationSize - 1; i++) {
-            generation.add(generator.generateFromExisting(originalAssignment));
-        }
-
-        generation.add(originalAssignment.copy()); // we also add original assignment
-
-        return new SimplePopulation(generation);
     }
 }
