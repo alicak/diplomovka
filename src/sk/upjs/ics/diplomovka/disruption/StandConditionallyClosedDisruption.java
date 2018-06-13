69a12ee9bdea0349d59c867d66d85e87b91bc6a3
Alica Kacengova <alica.kacengova@gmail.com>
2018-06-13 11:41:03 +0200

    fix error in conditional disruption

--- src/sk/upjs/ics/diplomovka/disruption/StandConditionallyClosedDisruption.java
+++ src/sk/upjs/ics/diplomovka/disruption/StandConditionallyClosedDisruption.java
@@ -1,6 +1,7 @@
 package sk.upjs.ics.diplomovka.disruption;
 
 import sk.upjs.ics.diplomovka.absolutechromosome.Chromosome;
+import sk.upjs.ics.diplomovka.data.GeneralStorage;
 import sk.upjs.ics.diplomovka.data.stands.closures.ClosureCondition;
 import sk.upjs.ics.diplomovka.data.stands.closures.ConditionalStandClosure;
 import sk.upjs.ics.diplomovka.data.stands.StandsStorage;
@@ -11,16 +12,18 @@ public class StandConditionallyClosedDisruption implements Disruption {
     private int id;
     private StandsStorage standsStorage;
     private ConditionalStandClosure closure;
+    private GeneralStorage storage;
 
-    public StandConditionallyClosedDisruption(int standId, int start, int end, ClosureCondition condition, StandsStorage standsStorage, int id) {
-        this.standsStorage = standsStorage;
+    public StandConditionallyClosedDisruption(int standId, int start, int end, ClosureCondition condition, GeneralStorage storage, int id) {
+        this.standsStorage = storage.getStandsStorage();
+        this.storage = storage;
         this.closure = new ConditionalStandClosure(standId, start, end, condition, id);
         this.id = id;
     }
 
     @Override
     public void disruptAssignment(Chromosome chromosome) {
-        chromosome.applyConditionalStandClosure(closure, standsStorage.getNumberById(closure.getStandId()));
+        chromosome.applyConditionalStandClosure(closure, standsStorage.getNumberById(closure.getStandId()), storage.getFlightStorage());
     }
 
     @Override
