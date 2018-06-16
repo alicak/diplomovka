1e595ca1fc5420640952c85076e178f80f626559
Alica Kacengova <alica.kacengova@gmail.com>
2018-06-16 10:39:53 +0200

    tidy up Main method (disruptions)

--- src/main/java/sk/upjs/ics/diplomovka/main/AbsoluteMain.java
+++ src/main/java/sk/upjs/ics/diplomovka/main/AbsoluteMain.java
@@ -37,19 +37,15 @@ public class AbsoluteMain {
 
         GeneralStorage storage = parser.parseDataFromJsons("categories.json", "aircrafts.json",
                 "engineTypes.json", "transfers.json", "gates.json", "gateDistances.json",
-                "standDistances.json", "stands.json", "departures.json"); // TODO
+                "standDistances.json", "stands.json", "departures.json");
         StandsStorage standsStorage = storage.getStandsStorage();
         FlightStorage flightStorage = storage.getFlightStorage();
 
-        parser.parseDisruptions("disruptionsExample.json", storage);
+        List<Disruption> disruptions = parser.parseDisruptions("disruptionsExample.json", storage);
 
-        Disruption gate5closed = new StandClosedDisruption(5, standsStorage, 1);
         Disruption gate6closed = new StandClosedDisruption(6, standsStorage, 2);
-        Disruption gate9closed = new StandClosedDisruption(9, standsStorage, 3);
         Disruption flight13cancelled = new FlightCancelledDisruption(13, flightStorage, 4);
         Disruption flight0delayed = new FlightDelayedDisruption(180, 1, flightStorage, 5); // no 2
-        Disruption flight34delayed = new FlightDelayedDisruption(60, 34, flightStorage, 6); // no 75
-        Disruption gate1tempClosed = new StandTemporarilyClosedDisruption(1, 500, 1000, standsStorage, 7);
         Disruption gate5tempClosed = new StandTemporarilyClosedDisruption(5, 300, 900, standsStorage, 8);
         Disruption gate7condTempClosed = new StandConditionallyClosedDisruption(7, 0, 1439,
                 new EngineTypeClosureCondition(Arrays.asList(1)), storage, 9);
@@ -61,56 +57,34 @@ public class AbsoluteMain {
         // here we would do that...
         GeneralStorage storage2 = storage.getStorageWithOptionalStart(100);
 
-        AssignmentCreator assignmentCreator = new AssignmentCreator(storage);
-        PopulationCreator populationCreator = new PopulationCreator();
-
-        // absolute position chromosome
         AbsolutePositionFeasibilityChecker feasibilityChecker = new AbsolutePositionFeasibilityChecker(storage);
+        AssignmentCreator assignmentCreator = new AssignmentCreator(storage);
         Chromosome originalAssignment = assignmentCreator.createAbsoluteOriginalAssignment(feasibilityChecker);
-
-        //System.out.println("original fitness 1: " + fitnessFunction.calculateFitness(originalAssignment));
-
-//        gate6closed.disruptAssignment(originalAssignment);
-//        gate9closed.disruptAssignment(originalAssignment);
-        //flight13cancelled.disruptAssignment(originalAssignment);
-//        flight0delayed.disruptAssignment(originalAssignment);
-//        flight34delayed.disruptAssignment(originalAssignment);
-//        gate5closed.disruptAssignment(originalAssignment);
-
         AbsolutePositionPopulation population = PopulationCreator.createAbsoluteInitialPopulation(generationSize, originalAssignment, feasibilityChecker, storage);
 
-        gate1tempClosed.disruptAssignment(originalAssignment); // this has to be done after population creation
+        // only if we need original assignment - otherwise it's already in population
+        gate6closed.disruptAssignment(originalAssignment);
         gate5tempClosed.disruptAssignment(originalAssignment);
         gate7condTempClosed.disruptAssignment(originalAssignment);
-
         flight13cancelled.disruptAssignment(originalAssignment);
         flight0delayed.disruptAssignment(originalAssignment);
-        flight34delayed.disruptAssignment(originalAssignment);
 
         for (Chromosome c : population.get()) {
-            //gate5closed.disruptAssignment(c);
-            //gate6closed.disruptAssignment(c);
-            //gate9closed.disruptAssignment(c);
-            gate1tempClosed.disruptAssignment(c);
+            gate6closed.disruptAssignment(c);
             gate5tempClosed.disruptAssignment(c);
             gate7condTempClosed.disruptAssignment(c);
 
             flight13cancelled.disruptAssignment(c);
             flight0delayed.disruptAssignment(c);
-            flight34delayed.disruptAssignment(c);
 
         }
 
-        //gate6closed.disruptStorage();
-        //gate9closed.disruptStorage();
-
-        flight13cancelled.disruptStorage();
-        flight0delayed.disruptStorage();
-        flight34delayed.disruptStorage();
-
-        gate1tempClosed.disruptStorage();
+        gate6closed.disruptStorage();
         gate5tempClosed.disruptStorage();
         gate7condTempClosed.disruptStorage();
+        flight13cancelled.disruptStorage();
+        flight0delayed.disruptStorage();
+
 
         //TimeDiffFitness fitnessFunction = new TimeDiffFitness(flightStorage);
         //ReassignmentFitness fitnessFunction = new ReassignmentFitness(flightStorage, standsStorage);
@@ -126,7 +100,6 @@ public class AbsoluteMain {
         CrossoverBase crossover = new AbsolutePositionCrossover(1);
         MutationBase mutation = new AbsolutePositionMutation(0.1);
 
-        //TerminationBase termination = new FitnessTermination(6000, population, fitnessFunction);
 
         // results
         AlgorithmBase algorithm = new Algorithm(population, fitnessFunction, crossover, mutation, selection, termination, storage);
@@ -134,8 +107,6 @@ public class AbsoluteMain {
         Chromosome result = finalPopulation.bestChromosome();
 
         System.out.println("original fitness: " + fitnessFunction.calculateFitness(originalAssignment));
-//        System.out.println("no of iterations: " + termination.getNoOfIterations());
-//        System.out.println("fitness: " + result.getFitness());
 
         FitnessFunctionBase timeDiffFitness = new TimeDiffFitness(storage, weights);
         FitnessFunctionBase reassignmentFitness = new ReassignmentFitness(storage, weights);
